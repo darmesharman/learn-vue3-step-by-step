84c2655a412368f7b2867f34cb24ec0229fb6440
Arman Darmesh <darmesharman@gmail.com>
2022-05-14 21:07:56 +0600

    Lifecycle Hooks, Fake APIs, and AJAX

--- js/components/Assignments.js
+++ js/components/Assignments.js
@@ -15,12 +15,16 @@ export default {
 
     data() {
         return {
-            assignments: [
-                {name: 'Finish projects', complete: false, id: 1, tag: 'math' },
-                {name: 'Read chapter 4', complete: false, id: 2, tag: 'science' },
-                {name: 'Turn in homework', complete: false, id: 3, tag: 'math' },
-            ],
+            assignments: [],
         }
+    },
+
+    created() {
+        fetch('http://localhost:3001/assignments')
+            .then(response => response.json())
+            .then(assignments => {
+                this.assignments = assignments;
+            });
     },
 
     computed: {
