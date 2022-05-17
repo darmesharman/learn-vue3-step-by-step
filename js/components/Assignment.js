c6eacf5a02e96fe590ec7556d7f92a509eeba813
Arman Darmesh <darmesharman@gmail.com>
2022-05-17 19:05:36 +0600

    master More Flexible Components With Slots and Flags

--- js/components/Assignment.js
+++ js/components/Assignment.js
@@ -1,11 +1,10 @@
 export default {
     template: `
         <li class="p-2 flex justify-between items-center">
-            <label>
+            <label class="block">
                 {{ assignment.name }}
-
-                <input type="checkbox" v-model="assignment.complete" class="ml-3">
             </label>
+            <input type="checkbox" v-model="assignment.complete" class="block ml-3">
         </li>
     `,
 
